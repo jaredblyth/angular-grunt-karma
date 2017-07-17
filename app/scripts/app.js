82a013e1f879b19ab27f7d661d4eaa0c9c53511e
jaredblyth <jaredblyth@hotmail.com>
2017-07-17 20:12:52 +1200

    Added toaster notifications

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'httpProviderConfig'
+    'httpProviderConfig',
+    'toaster'
   ])
   .config(function ($routeProvider) {
     $routeProvider
