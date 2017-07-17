7a688b010aaa4c47765425622d84a58c6fc0a243
jaredblyth <jaredblyth@hotmail.com>
2017-07-17 20:10:55 +1200

    Removed HTTP Caching

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'httpProviderConfig'
   ])
   .config(function ($routeProvider) {
     $routeProvider
